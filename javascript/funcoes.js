131552df47e76f63b3a5ca1e9daab772fc50abef
andresdiniz <69819014+andresdiniz@users.noreply.github.com>
2022-03-06 11:57:31 -0300

    Adiçoes

---
+++ javascript/funcoes.js
@@ -0,0 +1,24 @@
+var min, seg,mins,segs;		min = 02;		seg = 1		
+	function relogio(){			
+		if((min > 0) || (seg > 0)){				
+			if(seg == 0){					
+				seg = 59;					
+				min = min - 1	
+			}				
+			else{					
+				seg = seg - 1;				
+			}				
+			if(min.toString().length == 1){					
+				min = "0" + min;				
+			}				
+			if(seg.toString().length == 1){					
+				seg = "0" + seg;				
+			}				
+			document.getElementById('spanRelogio').innerHTML = min + ":" + seg;				
+			setTimeout('relogio()', 1000);			
+		}			
+		else{				
+			document.getElementById('spanRelogio').innerHTML = "00:00";			
+		}		
+	return mins,segs;
+    }	
