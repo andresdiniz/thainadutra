dd6e3240c281737a1ce73bd70de880c46d005c54
andresdiniz <69819014+andresdiniz@users.noreply.github.com>
2022-03-14 10:22:50 -0300

    update css

--- php/email.php
+++ php/email.php
@@ -1,12 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
+<!doctype htlm>
+<html lang="pt-br">
+    <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Muito Obrigado! </title>
+    <title>Google Glass - Cadastro </title>
+    <link rel="stylesheet" href="_css/estilo.css">
 </head>
 <body>
-    
+    <div>
+    <?php
+    $nome = $_POST["tName"];
+    $telefone = $_POST["tTel"];
+    $sexo = $_POST["TSexo"];
+    $curso = $_POST["tCurso"];
+
+    echo "O nome é $nome e o sexo é $sexo e o numero de telefone é $telefone, seu curso é $curso;
+?>
+</div>
 </body>
 </html>
