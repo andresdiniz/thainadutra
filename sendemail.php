01dd6e845bfdff3423f25c201156584ffcb6db58
andresdiniz <69819014+andresdiniz@users.noreply.github.com>
2022-03-17 19:11:49 -0300

    Update sendemail.php

--- sendemail.php
+++ sendemail.php
@@ -2,7 +2,7 @@
 ini_set('display_errors',1);
 error_reporting( E_ALL );
 $from ="test@hostinger-tutorials.com";
-$to = andresoaresdiniz201218@gmail.com;
+$to = "andresoaresdiniz201218@gmail.com";
 $subject = "Checking PHP mail";
 $message = "PHP mail works just fine";
 $headers = "From:" . $from;
