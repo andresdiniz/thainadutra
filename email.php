3f69c5cd95f2f38ed6a89c235b072c0ddc19b62a
andresdiniz <69819014+andresdiniz@users.noreply.github.com>
2022-03-17 20:37:12 -0300

    email

--- email.php
+++ email.php
@@ -21,15 +21,28 @@
     if ($conn->query($sql) === TRUE) {
       include('check.html');
         // echo "Olá $nome, muito bem em breve você recebera seu ebook gratuitamente no seu email.$result</br>";
-        ini_set('display_errors',1);
-        error_reporting( E_ALL );
-        $from ="sac@thainadutra.com.br";
-        $to = $email;
-        $subject = "Curso gratuito";
-        $message = message.html;
-        $headers = "From:" . $from;
-        mail($to,$subject,$message,$headers);
-        echo "Email enviado corretamente";
+        use PHPMailer\PHPMailer\PHPMailer;
+        require 'vendor/autoload.php';
+        $mail = new PHPMailer;
+        $mail->isSMTP();
+        $mail->SMTPDebug = 2;
+        $mail->Host = 'smtp.hostinger.com';
+        $mail->Port = 465;
+        $mail->SMTPAuth = true;
+        $mail->Username = 'no-reply@thainadutra.com.br';
+        $mail->Password = '30012022@ndrE';
+        $mail->setFrom('no-reply@thainadutra.com.br', 'Ebook');
+        $mail->addReplyTo('sac@thainadutra.com.br', 'Sac');
+        $mail->addAddress($email, $nome);
+        $mail->Subject = 'Ebook gratuito';
+        $mail->msgHTML(file_get_contents('message.html'), __DIR__);
+        $mail->Body = 'This is a plain text message body';
+//$mail->addAttachment('test.txt');
+if (!$mail->send()) {
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'The email message was sent.';
+}
       } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
       }
