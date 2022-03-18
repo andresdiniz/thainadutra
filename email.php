c5cf974ffa6d8fba11a9b64f7a77f9810f6c6105
andresdiniz <69819014+andresdiniz@users.noreply.github.com>
2022-03-18 16:09:44 -0300

    master

--- email.php
+++ email.php
@@ -12,37 +12,39 @@
     
     $timezone = new DateTimeZone("America/Sao_Paulo");
     $agora = new DateTime('now', $timezone);
-    $result = $agora->format("d-m-Y");
+    $result = $agora->format("Y-m-d H:i:s ");
 
     // $texto = Nome: $nome</br> Email: $email;
-    // echo $texto;
-    $sql = "INSERT INTO leads (ID, Nome, Email, dataatual) VALUES (NULL, '$nome','$email',$result)";
+    $sql = "INSERT INTO leads (ID, Nome, Email, dataatual) VALUES (NULL, '$nome','$email','$result')";
     
     if ($conn->query($sql) === TRUE) {
       include('check.html');
-        // echo "Olá $nome, muito bem em breve você recebera seu ebook gratuitamente no seu email.$result</br>";
-        use PHPMailer\PHPMailer\PHPMailer;
-        require 'vendor/autoload.php';
-        $mail = new PHPMailer;
-        $mail->isSMTP();
-        $mail->SMTPDebug = 2;
-        $mail->Host = 'smtp.hostinger.com';
-        $mail->Port = 465;
-        $mail->SMTPAuth = true;
-        $mail->Username = 'no-reply@thainadutra.com.br';
-        $mail->Password = '30012022@ndrE';
-        $mail->setFrom('no-reply@thainadutra.com.br', 'Ebook');
-        $mail->addReplyTo('sac@thainadutra.com.br', 'Sac');
-        $mail->addAddress($email, $nome);
-        $mail->Subject = 'Ebook gratuito';
-        $mail->msgHTML(file_get_contents('message.html'), __DIR__);
-        $mail->Body = 'This is a plain text message body';
-//$mail->addAttachment('test.txt');
-if (!$mail->send()) {
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-    echo 'The email message was sent.';
-}
+      // ENviar email..
+
+          use PHPMailer\PHPMailer\PHPMailer;
+          require 'vendor/autoload.php';
+          $mail = new PHPMailer;
+          $mail->isSMTP();
+          $mail->SMTPDebug = 2;
+          $mail->Host = 'smtp.hostinger.com';
+          $mail->Port = 587;
+          $mail->SMTPAuth = true;
+          $mail->Username = 'sac@thainadutra.com.br';
+          $mail->Password = '30012022@ndrE';
+          $mail->setFrom('sac@thainadutra.com.br', 'Thaina');
+          $mail->addReplyTo('no-reply@thainadutra.com.br', 'Nao responda');
+          $mail->addAddress($email, $nome);
+          $mail->Subject = 'Testing PHPMailer';
+          //$mail->msgHTML(file_get_contents('message.html'), __DIR__);
+          $mail->Body = 'This is a plain text message body';
+          //$mail->addAttachment('test.txt');
+          if (!$mail->send()) {
+              echo 'Mailer Error: ' . $mail->ErrorInfo;
+          } else {
+              echo 'The email message was sent.';
+          }
+
+// Fim enviar email
       } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
       }
